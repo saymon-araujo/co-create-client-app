b6a2be9fc1b08ad2563fbede5b4064000ce54978
Saymon Araújo <67705166+saymon-araujo@users.noreply.github.com>
2024-09-26 10:29:01 -0300

    feature: first step UI

--- app/purchases.tsx
+++ app/purchases.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Header } from "@/components";
 import { PDPActionButton } from "@/components/compound/PDPActionButton";
 import { useLocalSearchParams } from "expo-router";
-import { ScrollView, View } from "tamagui";
+import { H2, H3, Input, Label, Paragraph, ScrollView, View, YStack } from "tamagui";
 
 export default function PurchasesScreen() {
   const { checkoutId } = useLocalSearchParams();
@@ -26,8 +26,199 @@ export default function PurchasesScreen() {
 
   return (
     <View flex={1} bg={"$white1"}>
-      <Header goBack />
-      <ScrollView showsVerticalScrollIndicator={false}></ScrollView>
+      <Header goBack step={1} />
+      <ScrollView showsVerticalScrollIndicator={false} mx={16}>
+        <H2 fontWeight={"bold"} ta={"left"} mt={16}>
+          Enter Contact{"\n"}Information
+        </H2>
+
+        <Paragraph color={"$gray11"} mt={4} mb={16}>
+          To schedule your session, we need to collect some essential information.
+        </Paragraph>
+        <YStack>
+          <H3 fontWeight={"bold"} ta={"left"}>
+            Personal Details
+          </H3>
+          <Paragraph color={"$gray11"} mb={8}>
+            Your artist will use these details to address you properly.
+          </Paragraph>
+
+          {/* // This could be componetinzed to recude amount of lines, but because it's a prototype I'll keep it here. */}
+          <YStack alignItems="flex-start" space="$2">
+            <Input
+              w={"100%"}
+              placeholder={`Your first name`}
+              h={80}
+              borderRadius={0}
+              bg={"$white1"}
+              borderColor={"$gray7"}
+              borderLeftColor={"$white1"}
+              borderRightColor={"$white1"}
+              fontSize={14}
+            />
+            <Label position="absolute" top={-2} left={16} fontSize={12} letterSpacing={1.6}>
+              FIRST NAME
+            </Label>
+          </YStack>
+
+          <YStack alignItems="flex-start" space="$2">
+            <Input
+              w={"100%"}
+              placeholder={`Your last name`}
+              h={80}
+              borderRadius={0}
+              bg={"$white1"}
+              borderColor={"$gray7"}
+              borderLeftColor={"$white1"}
+              borderRightColor={"$white1"}
+              fontSize={14}
+            />
+            <Label position="absolute" top={-2} left={16} fontSize={12} letterSpacing={1.6}>
+              LAST NAME
+            </Label>
+          </YStack>
+
+          <YStack alignItems="flex-start" space="$2">
+            <Input
+              w={"100%"}
+              placeholder={`Your last name`}
+              h={80}
+              borderRadius={0}
+              bg={"$white1"}
+              borderColor={"$gray7"}
+              borderLeftColor={"$white1"}
+              borderRightColor={"$white1"}
+              fontSize={14}
+            />
+            <Label position="absolute" top={-2} left={16} fontSize={12} letterSpacing={1.6}>
+              PRONOUNS (OPTIONAL)
+            </Label>
+          </YStack>
+        </YStack>
+
+        <YStack mt={16}>
+          <H3 fontWeight={"bold"} ta={"left"}>
+            Phone Number
+          </H3>
+          <Paragraph color={"$gray11"} mb={8}>
+            The artist may use this number to contact you for anything urgent regarding your
+            appointment.
+          </Paragraph>
+
+          {/* // Didn't implement the select to gain some time. */}
+          <YStack alignItems="flex-start" space="$2">
+            <Input
+              w={"100%"}
+              placeholder={`Your country code`}
+              h={80}
+              borderRadius={0}
+              bg={"$white1"}
+              borderColor={"$gray7"}
+              borderLeftColor={"$white1"}
+              borderRightColor={"$white1"}
+              fontSize={14}
+            />
+            <Label position="absolute" top={-2} left={16} fontSize={12} letterSpacing={1.6}>
+              COUNTRY CODE
+            </Label>
+          </YStack>
+
+          {/* // A input mask is necessary here as well */}
+          <YStack alignItems="flex-start" space="$2">
+            <Input
+              w={"100%"}
+              placeholder={`Your last name`}
+              h={80}
+              borderRadius={0}
+              bg={"$white1"}
+              borderColor={"$gray7"}
+              borderLeftColor={"$white1"}
+              borderRightColor={"$white1"}
+              fontSize={14}
+            />
+            <Label position="absolute" top={-2} left={16} fontSize={12} letterSpacing={1.6}>
+              PHONE NUMBER
+            </Label>
+          </YStack>
+
+          <YStack alignItems="flex-start" space="$2">
+            <Input
+              w={"100%"}
+              placeholder={`Your last name`}
+              h={80}
+              borderRadius={0}
+              bg={"$white1"}
+              borderColor={"$gray7"}
+              borderLeftColor={"$white1"}
+              borderRightColor={"$white1"}
+              fontSize={14}
+            />
+            <Label position="absolute" top={-2} left={16} fontSize={12} letterSpacing={1.6}>
+              PRONOUNS (OPTIONAL)
+            </Label>
+          </YStack>
+        </YStack>
+
+        <YStack mt={16} mb={32}>
+          <H3 fontWeight={"bold"} ta={"left"}>
+            Your Location
+          </H3>
+          <Paragraph color={"$gray11"} mb={8}>
+            This helps your artist understand if you are traveling a long distance for your session.
+          </Paragraph>
+
+          <YStack alignItems="flex-start" space="$2">
+            <Input
+              w={"100%"}
+              placeholder={`Your Country`}
+              h={80}
+              borderRadius={0}
+              bg={"$white1"}
+              borderColor={"$gray7"}
+              borderLeftColor={"$white1"}
+              borderRightColor={"$white1"}
+              fontSize={14}
+            />
+            <Label position="absolute" top={-2} left={16} fontSize={12} letterSpacing={1.6}>
+              COUNTRY
+            </Label>
+          </YStack>
+
+          <YStack alignItems="flex-start" space="$2">
+            <Input
+              w={"100%"}
+              placeholder={`Your State`}
+              h={80}
+              borderRadius={0}
+              bg={"$white1"}
+              borderColor={"$gray7"}
+              borderLeftColor={"$white1"}
+              borderRightColor={"$white1"}
+              fontSize={14}
+            />
+            <Label position="absolute" top={-2} left={16} fontSize={12} letterSpacing={1.6}>
+              STATE
+            </Label>
+          </YStack>
+
+          <YStack alignItems="flex-start" space="$2">
+            <Input
+              w={"100%"}
+              placeholder={`Your City`}
+              h={80}
+              borderRadius={0}
+              bg={"$white1"}
+              borderColor={"$gray7"}
+              borderLeftColor={"$white1"}
+              borderRightColor={"$white1"}
+              fontSize={14}
+            />
+            <Label position="absolute" top={-2} left={16} fontSize={12} letterSpacing={1.6}>
+              CITY
+            </Label>
+          </YStack>
+        </YStack>
+      </ScrollView>
 
       <PDPActionButton title="SAVE" onPress={handleStartCheckout} isLoading={isLoading} />
     </View>
--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -1,48 +1,110 @@
-import React from "react";
+import React, { useEffect, useState, useCallback, useRef, useMemo } from "react";
 import { screen } from "@/constants/screen";
-import { View, XStack } from "tamagui";
+import { Text, View, XStack } from "tamagui";
 import { Image } from "expo-image";
-import { ChevronLeft } from "lucide-react-native";
+import { ArrowLeft } from "lucide-react-native";
 import { TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
 
 interface Props {
   goBack?: boolean;
+  step?: 1 | 2 | 3;
+  initialTime?: number;
 }
 
-export function Header({ goBack }: Props) {
+export function Header({ goBack, step, initialTime = 300 }: Props) {
   const router = useRouter();
   const imageSizeMultiplier = 0.09;
 
-  function handleGoBack() {
+  const [timeRemaining, setTimeRemaining] = useState(initialTime);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  const handleGoBack = useCallback(() => {
     if (goBack) {
       router.back();
     }
-  }
+  }, [goBack, router]);
+
+  const formatTime = useCallback((seconds: number) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
+  }, []);
+
+  const formattedTime = useMemo(() => formatTime(timeRemaining), [timeRemaining, formatTime]);
+
+  useEffect(() => {
+    if (timeRemaining <= 0) {
+      return;
+    }
+
+    if (!intervalRef.current) {
+      intervalRef.current = setInterval(() => {
+        setTimeRemaining((prevTime) => {
+          if (prevTime <= 1) {
+            clearInterval(intervalRef.current!);
+            intervalRef.current = null;
+            return 0;
+          }
+          return prevTime - 1;
+        });
+      }, 1000);
+    }
+
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+    };
+  }, [timeRemaining]);
 
   return (
-    <View pb={16} pt={16 + screen.statusBarHeight} px={16} bg={"$white1"}>
-      <XStack gap={16} alignItems="center">
-        {goBack && (
-          <TouchableOpacity
-            onPress={handleGoBack}
-            hitSlop={{ bottom: 8, left: 8, right: 8, top: 8 }}
-          >
-            <View borderWidth={1.5} borderRadius={4} p={1} borderColor={"black"}>
-              <ChevronLeft color={"#000"} />
-            </View>
-          </TouchableOpacity>
-        )}
+    <>
+      <View
+        pb={16}
+        pt={16 + screen.statusBarHeight}
+        px={16}
+        bg={"$white1"}
+        borderBottomWidth={1}
+        borderColor={"$gray5"}
+      >
+        <XStack gap={16} alignItems="center" justifyContent={step ? "space-between" : "flex-start"}>
+          {goBack && (
+            <TouchableOpacity
+              onPress={handleGoBack}
+              hitSlop={{ bottom: 8, left: 8, right: 8, top: 8 }}
+            >
+              <ArrowLeft color={"#000"} />
+            </TouchableOpacity>
+          )}
 
-        <Image
-          cachePolicy={"disk"}
-          source={require("@/assets/images/cocreate-logo-dark.png")}
-          style={{
-            width: 1600 * imageSizeMultiplier,
-            height: 400 * imageSizeMultiplier,
-          }}
-        />
-      </XStack>
-    </View>
+          {!step ? (
+            <Image
+              cachePolicy={"disk"}
+              source={require("@/assets/images/cocreate-logo-dark.png")}
+              style={{
+                width: 1600 * imageSizeMultiplier,
+                height: 400 * imageSizeMultiplier,
+              }}
+            />
+          ) : (
+            <Text color={"$gray10"} letterSpacing={1.4} fontSize={12}>
+              STEP {step}/3
+            </Text>
+          )}
+        </XStack>
+      </View>
+
+      {step && (
+        <>
+          <View bg={"$gray2"} py={6}>
+            <Text ta={"center"} letterSpacing={1.2}>
+              TIME REMAINING {formattedTime}
+            </Text>
+          </View>
+        </>
+      )}
+    </>
   );
 }
